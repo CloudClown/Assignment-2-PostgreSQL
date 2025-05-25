3fd18831943833e0f52c7988f61ebe5e6f2de78c
Tahmid <tahmid.chowdhury@sjinnovation.com>
2025-05-25 17:09:55 +0600

    final updated code for table sightings

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -22,8 +22,8 @@ CREATE TABLE species (
 -- Create sightings table
 CREATE TABLE sightings (
     sighting_id SERIAL PRIMARY KEY,
-    ranger_id INT REFERENCES rangers(ranger_id) ON DELETE SET NULL,
-    species_id INT REFERENCES species(species_id) ON DELETE SET NULL,
+    ranger_id INT NULL REFERENCES rangers(ranger_id) ON DELETE SET NULL,
+    species_id INT NOT NULL REFERENCES species(species_id),
     sighting_time TIMESTAMP NOT NULL,
     location VARCHAR(150) NOT NULL,
     notes TEXT
